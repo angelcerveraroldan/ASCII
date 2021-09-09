de0a1cac0bacdb1b19f966ab5562d25a80f351f3
Angel Cervera Roldan <48255007+AngelCerveraRoldan@users.noreply.github.com>
2021-09-09 21:35:05 +0100

    Moved resize function into makeBlackAndWhite

--- src/main/scala/ImageAscii.scala
+++ src/main/scala/ImageAscii.scala
@@ -1,28 +1,40 @@
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import java.awt.Color
-import java.awt.Image
+import java.awt.{Color, Graphics2D, Image, RenderingHints}
 
 
 // TURN IMAGE TO ASCII
-class ImageAscii(imageName: String) {
-  val projectPath: String = new java.io.File(".").getCanonicalPath
-  val image: BufferedImage = ImageIO.read(new File(s"$projectPath/src/images/$imageName"))
+class ImageAscii(image: BufferedImage, desiredWidth: Int) {
 
+  def makeBlackAndWhiteImg(blackAndWhiteImageName: String): BufferedImage = {
+    def resize(image: BufferedImage, newWidth: Int): BufferedImage = {
+      val ratio: Float = newWidth.toFloat / image.getWidth.toFloat
+      val newHeight: Int = (image.getHeight.toFloat * ratio).toInt
 
-  def makeBlackAndWhiteImg(blackAndWhiteImageName: String): Boolean = {
-    val blackAndWhiteImage: BufferedImage = new BufferedImage(image.getWidth, image.getHeight, BufferedImage.TYPE_BYTE_GRAY)
-    blackAndWhiteImage.createGraphics.drawImage(image, 0, 0, Color.WHITE, null)
+      // Get type will find out what type the image is
+      val dimg = new BufferedImage(newWidth, newHeight, image.getType)
+      val g: Graphics2D = dimg.createGraphics()
+      g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+        RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+      g.drawImage(image, 0, 0, newWidth, newHeight, 0, 0, image.getWidth, image.getHeight, null)
+      g.dispose()
 
-    ImageIO.write(blackAndWhiteImage, "jpg", new File(s"src/images/$blackAndWhiteImageName.jpg"))
+      dimg
+    }
+
+    val sizedImage: BufferedImage = if (desiredWidth != image.getWidth) {
+      resize(image, desiredWidth)
+    } else image
+
+    val blackAndWhiteImage: BufferedImage = new BufferedImage(sizedImage.getWidth, sizedImage.getHeight, BufferedImage.TYPE_BYTE_GRAY)
+    blackAndWhiteImage.createGraphics.drawImage(sizedImage, 0, 0, Color.WHITE, null)
+
+    // TODO: Remove this when tests are implemented
+    ImageIO.write(blackAndWhiteImage, "jpeg", new File(s"src/images/$blackAndWhiteImageName.jpeg"))
+
+    blackAndWhiteImage
   }
 
-  def resizeImage(newWidth: Int = 1910): Image = {
-    val ratio: Float = newWidth.toFloat / image.getWidth.toFloat
-    val newHeight: Int = (image.getHeight.toFloat * ratio).toInt
-
-    image.getScaledInstance(newWidth, newHeight, Image.SCALE_FAST)
-  }
 
 }
