623c4a4fb519ee1ceccd09c61be6e633ed9f70f7
Angel Cervera Roldan <48255007+AngelCerveraRoldan@users.noreply.github.com>
2021-09-10 08:07:53 +0100

    Moved all code into one function

--- src/main/scala/ImageAscii.scala
+++ src/main/scala/ImageAscii.scala
@@ -1,39 +1,25 @@
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import java.awt.{Color, Graphics2D, Image, RenderingHints}
-
+import java.awt.{Graphics2D, RenderingHints}
 
 // TURN IMAGE TO ASCII
 class ImageAscii(image: BufferedImage, desiredWidth: Int) {
+  val ratio: Float = desiredWidth.toFloat / image.getWidth.toFloat
+  val newHeight: Int = (image.getHeight.toFloat * ratio).toInt
 
-  def makeBlackAndWhiteImg(blackAndWhiteImageName: String): BufferedImage = {
-    def resize(image: BufferedImage, newWidth: Int): BufferedImage = {
-      val ratio: Float = newWidth.toFloat / image.getWidth.toFloat
-      val newHeight: Int = (image.getHeight.toFloat * ratio).toInt
-
-      // Get type will find out what type the image is
-      val dimg = new BufferedImage(newWidth, newHeight, image.getType)
-      val g: Graphics2D = dimg.createGraphics()
-      g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-        RenderingHints.VALUE_INTERPOLATION_BILINEAR)
-      g.drawImage(image, 0, 0, newWidth, newHeight, 0, 0, image.getWidth, image.getHeight, null)
-      g.dispose()
-
-      dimg
-    }
-
-    val sizedImage: BufferedImage = if (desiredWidth != image.getWidth) {
-      resize(image, desiredWidth)
-    } else image
-
-    val blackAndWhiteImage: BufferedImage = new BufferedImage(sizedImage.getWidth, sizedImage.getHeight, BufferedImage.TYPE_BYTE_GRAY)
-    blackAndWhiteImage.createGraphics.drawImage(sizedImage, 0, 0, Color.WHITE, null)
+  def refactorImage(newImageName: String, image: BufferedImage = image): BufferedImage = {
+    // This is where the image is both resized and made into a black and white image vvv
+    val sizedImage = new BufferedImage(desiredWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY)
+    val g: Graphics2D = sizedImage.createGraphics()
+    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+    g.drawImage(image, 0, 0, desiredWidth, newHeight, 0, 0, image.getWidth, image.getHeight, null)
+    g.dispose()
 
     // TODO: Remove this when tests are implemented
-    ImageIO.write(blackAndWhiteImage, "jpeg", new File(s"src/images/$blackAndWhiteImageName.jpeg"))
-
-    blackAndWhiteImage
+    ImageIO.write(sizedImage, "jpeg", new File(s"src/images/$newImageName.jpeg"))
+    
+    sizedImage
   }
 
 
--- src/main/scala/ShowImage.scala
+++ src/main/scala/ShowImage.scala
@@ -2,14 +2,13 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-//.makeBlackAndWhiteImg("blackAndWhite")
 object ShowImage extends App {
   val imageName: String = "eagle.jpeg"
-
   val projectPath: String = new java.io.File(".").getCanonicalPath
-  val originalImage: BufferedImage = ImageIO.read(new File(s"$projectPath/src/images/$imageName"))
 
-  val imageAscii: BufferedImage = new ImageAscii(originalImage, 300).makeBlackAndWhiteImg("bAndW")
-  
+  val originalImage: BufferedImage = ImageIO.read(new File(s"$projectPath/src/images/$imageName"))
+  val imageAscii: BufferedImage = new ImageAscii(originalImage, 300).refactorImage("newImg")
+
+
 }
 
