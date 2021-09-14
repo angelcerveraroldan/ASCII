f2fda5471941c12ef7a083971a35ac9e446de250
Angel Cervera Roldan <48255007+AngelCerveraRoldan@users.noreply.github.com>
2021-09-14 11:52:41 +0100

    Image ready to be converted to ASCII

--- src/main/scala/ImageAscii.scala
+++ src/main/scala/ImageAscii.scala
@@ -4,11 +4,11 @@ import javax.imageio.ImageIO
 import java.awt.{Graphics2D, RenderingHints}
 
 // TURN IMAGE TO ASCII
-class ImageAscii(image: BufferedImage, desiredWidth: Int) {
-  val ratio: Float = desiredWidth.toFloat / image.getWidth.toFloat
-  val newHeight: Int = (image.getHeight.toFloat * ratio).toInt
+object ImageAscii {
+  def prepareImage(newImageName: String, image: BufferedImage, desiredWidth: Int): BufferedImage = {
+    val ratio: Float = desiredWidth.toFloat / image.getWidth.toFloat
+    val newHeight: Int = (image.getHeight.toFloat * ratio).toInt
 
-  def refactorImage(newImageName: String, image: BufferedImage = image): BufferedImage = {
     // This is where the image is both resized and made into a black and white image vvv
     val sizedImage = new BufferedImage(desiredWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY)
     val g: Graphics2D = sizedImage.createGraphics()
@@ -16,11 +16,36 @@ class ImageAscii(image: BufferedImage, desiredWidth: Int) {
     g.drawImage(image, 0, 0, desiredWidth, newHeight, 0, 0, image.getWidth, image.getHeight, null)
     g.dispose()
 
-    // TODO: Remove this when tests are implemented
-    ImageIO.write(sizedImage, "jpeg", new File(s"src/images/$newImageName.jpeg"))
-    
     sizedImage
   }
 
+  def printImage(x: Int, y: Int, image: BufferedImage): Any = {
+    test(image)
+  }
 
+  // TODO: Improve this function (it was copy pasted from java)
+  // Print the RGB value of each pixel in the image
+  def test(image: BufferedImage): Unit = {
+    var y = 0
+    while ( {
+      y < image.getHeight
+    }) {
+      var x = 0
+      while ( {
+        x < image.getWidth
+      }) {
+        val pixel = image.getRGB(x, y)
+        val alpha = (pixel >> 24) & 0xFF
+        val red = (pixel >> 16) & 0xFF
+        val green = (pixel >> 8) & 0xFF
+        val blue = pixel & 0xFF
+
+        println(s"(R,G,B) -> ($red, $green, $blue)")
+
+        x += 1
+      }
+
+      y += 1
+    }
+  }
 }
--- src/main/scala/ShowImage.scala
+++ src/main/scala/ShowImage.scala
@@ -2,13 +2,17 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+import ImageAscii._
+
 object ShowImage extends App {
   val imageName: String = "eagle.jpeg"
   val projectPath: String = new java.io.File(".").getCanonicalPath
 
   val originalImage: BufferedImage = ImageIO.read(new File(s"$projectPath/src/images/$imageName"))
-  val imageAscii: BufferedImage = new ImageAscii(originalImage, 300).refactorImage("newImg")
+  val sizedImage: BufferedImage = prepareImage("newImg", originalImage, 300)
+  ImageIO.write(sizedImage, "jpeg", new File(s"src/images/newImage.jpeg"))
 
 
+  printImage(0, 0, sizedImage)
 }
 
